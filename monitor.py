1f58a6580211d6a7a00dc911e72393d7d8a682d4
Ahmed Elamen <ahmedelamen1989@gmail.com>
2026-08-08 14:13:39 +0300

    Add disk usage monitoring

--- monitor.py
+++ monitor.py
@@ -16,6 +16,13 @@ used_ram = memory.used / (1024 ** 3)
 free_ram = memory.free / (1024 ** 3)
 memory_usage = memory.percent
 
+disk = psutil.disk_usage("/")
+
+total_disk = disk.total / (1024 ** 3)
+used_disk = disk.used / (1024 ** 3)
+free_disk = disk.free / (1024 ** 3)
+disk_usage = disk.percent
+
 print ("Hostname :" , hostname)
 print ("Current User :" , current_user)
 print ("Current Date & Time :", current_datetime) 
@@ -27,3 +34,8 @@ print("Total :", round(total_ram, 2), "GB")
 print("Used :", round(used_ram, 2), "GB")
 print("Free :", round(free_ram, 2), "GB")
 print("Usage :", memory_usage, "%")
+print("Disk Usage:")
+print("Total :", round(total_disk, 2), "GB")
+print("Used :", round(used_disk, 2), "GB")
+print("Free :", round(free_disk, 2), "GB")
+print("Usage :", disk_usage, "%")
