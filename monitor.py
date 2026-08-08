4c860a433c8f9f7a94b2b7a226ad90830f9a7818
Ahmed Elamen <ahmedelamen1989@gmail.com>
2026-08-08 13:32:45 +0300

    Add date and time monitoring

--- monitor.py
+++ monitor.py
@@ -1,7 +1,10 @@
 import socket
 import getpass
+from datetime import datetime
+
 hostname = socket.gethostname()
 current_user = getpass.getuser()
-
+current_datetime = datetime.now()
 print ("Hostname :" , hostname)
-print("Current user :" , current_user)
+print ("Current User :" , current_user)
+print ("Current Date & Time :", current_datetime) 
