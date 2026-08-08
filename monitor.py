56c67a85ac0055379491497c297062d171e693bd
Ahmed Elamen <ahmedelamen1989@gmail.com>
2026-08-08 13:43:13 +0300

    Add operating system and kernal monitoring

--- monitor.py
+++ monitor.py
@@ -1,10 +1,17 @@
 import socket
 import getpass
 from datetime import datetime
+import platform
 
 hostname = socket.gethostname()
 current_user = getpass.getuser()
 current_datetime = datetime.now()
+operating_system = platform.system()
+kernal_version = platform.release()
+
 print ("Hostname :" , hostname)
 print ("Current User :" , current_user)
 print ("Current Date & Time :", current_datetime) 
+print ("Operating System :" , operating_system)
+print ("Kernal Version :" , kernal_version)
+
