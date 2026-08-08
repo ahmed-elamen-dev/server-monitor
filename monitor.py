785baf56633420e7dabb3e677a888c653e88b39e
Ahmed Elamen <ahmedelamen1989@gmail.com>
2026-08-08 13:55:53 +0300

    Add CPU usage monitoring

--- monitor.py
+++ monitor.py
@@ -2,16 +2,18 @@ import socket
 import getpass
 from datetime import datetime
 import platform
+import psutil
 
 hostname = socket.gethostname()
 current_user = getpass.getuser()
 current_datetime = datetime.now()
 operating_system = platform.system()
-kernal_version = platform.release()
+kernel_version = platform.release()
+cpu_usage = psutil.cpu_percent(interval=1)
 
 print ("Hostname :" , hostname)
 print ("Current User :" , current_user)
 print ("Current Date & Time :", current_datetime) 
 print ("Operating System :" , operating_system)
-print ("Kernal Version :" , kernal_version)
-
+print ("Kernel Version :" , kernel_version)
+print ("CPU Usage :" ,cpu_usage, "%")
